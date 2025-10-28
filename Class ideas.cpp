2cd089b993955262324c888e409df9acd21d1e26
ESDunlap <dunlape3@my.erau.edu>
2025-10-28 11:16:29 -0700

    Finished Shuffle.cpp

--- Class ideas.cpp
+++ Class ideas.cpp
@@ -2,6 +2,8 @@
 #include <climits> //Adds INT_MAX
 #include <string> //Allows use of string functions
 #include <iomanip> //Input Output additions
+#include <iostream>
+#include <fstream>
 
 
 using namespace std;
@@ -47,9 +49,15 @@ public:
 		}
 		this->plus = plus;
 	}
-	int getRank() { return rank; }
-	string getSuit() { return suit; }
-	bool getPlus() { return plus; }
+	int getRank() {
+		return rank;
+	}
+	string getSuit() {
+		return suit;
+	}
+	bool getPlus() {
+		return plus;
+	}
 	void onPlay();
 
 };
@@ -83,61 +91,77 @@ public:
 
 void Deck::fillDeck()
 {
-    //number cards
-    for(int rank = 0; rank < 10; rank++)
-    {
-        for(int color = 0; color < 4; color++)
-        {
-            cards[color + rank * 4] = BasicCard(rank, color + 1);
-            if (rank != 0)
-                cards[40 + color + (rank - 1) * 4] = BasicCard(rank, color + 1);
-        }
-    }
-    //plus cards
-    for(int color = 0; color < 4; color++)
-    {
-        cards[76 + color] = BasicCard(2, color + 1, true);
-        cards[80 + color] = BasicCard(2, color + 1, true);
-    }
-    //wild cards
-    for(int wildNumber = 0; wildNumber < 4; wildNumber++)
-    {
-        cards[84 + wildNumber] = WildCard();
-        cards[88 + wildNumber] = WildCard(true);
-    }
+	//number cards
+	for(int rank = 0; rank < 10; rank++)
+	{
+		for(int color = 0; color < 4; color++)
+		{
+			cards[color + rank * 4] = BasicCard(rank, color + 1);
+			if (rank != 0)
+				cards[40 + color + (rank - 1) * 4] = BasicCard(rank, color + 1);
+		}
+	}
+	//plus cards
+	for(int color = 0; color < 4; color++)
+	{
+		cards[76 + color] = BasicCard(2, color + 1, true);
+		cards[80 + color] = BasicCard(2, color + 1, true);
+	}
+	//wild cards
+	for(int wildNumber = 0; wildNumber < 4; wildNumber++)
+	{
+		cards[84 + wildNumber] = WildCard();
+		cards[88 + wildNumber] = WildCard(true);
+	}
 };
 
-//NOTE: DOES NOT CURRENTLY WORK. NEED TO FIND HOW TO CHECK IF AN ARRAY SPOT IS EMPTY
 void Deck::shuffle()
 {
-    Card shuffledCards[numCards];
-    int chosenSlot;
-    bool slotChosen=false;
-    int slots;
-    for(int chosenCard;chosenCard < numCards; chosenCard++)
-    {
-        do
-        {
-            chosenSlot= rand() % numCards;
-            //if(shuffledCards[chosenSlot]) //NEEDS TO BE FIXED
-            //{
-                shuffledCards[chosenSlot] = cards[chosenCard];
-                slotChosen= true;
-            //}
-            
-        }
-        while(slotChosen== false);
-        slotChosen=false;
-    }
-    for(int randomSpot;randomSpot < numCards; randomSpot++)
-    {
-        cards[randomSpot]= shuffledCards[randomSpot];
-    }
+	Card shuffledCards[numCards];
+	int alreadyChosenSlots[numCards];
+	int chosenSlot;
+	bool slotChosen = false;
+	bool alreadyBeenChosen = false;
+	int slots;
+	for(int chosenCard= 0; chosenCard < numCards; chosenCard++)
+	{
+		do
+		{
+			chosenSlot= rand() % numCards;
+			for(int alreadyChosen: alreadyChosenSlots)
+			{
+				if (chosenSlot == alreadyChosen)
+				{
+					alreadyBeenChosen= true;
+				}
+			}
+			if(alreadyBeenChosen==false)
+			{
+				shuffledCards[chosenSlot] = cards[chosenCard];
+				alreadyChosenSlots[chosenCard]= chosenSlot;
+				slotChosen= true;
+			}
+			alreadyBeenChosen= false;
+
+		}
+		while(slotChosen== false);
+		slotChosen=false;
+	}
+	for(int randomSpot = 0; randomSpot < numCards; randomSpot++)
+	{
+		cards[randomSpot]= shuffledCards[randomSpot];
+	}
+	for(int alreadyChosen: alreadyChosenSlots)
+	{
+		cout<<alreadyChosen<<endl;
+	}
 }
 
 int main()
 {
-    srand(time(0));
-    Deck newDeck(102);
-    newDeck.fillDeck();
+	srand(time(0));
+	Deck newDeck(88);
+	newDeck.fillDeck();
+	newDeck.shuffle();
+	return 0;
 }
