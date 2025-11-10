d084e84e1ea56667168a88687a1a3c89358356ee
Kaeden-D <dalyk3@my.erau.edu>
2025-11-10 15:52:18 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -11,7 +11,10 @@ class Player
 private:
 	vector<Card> hand;
 	int size;
-public:
+public: 
+	int getSize() { return size; }
 	void giveCard(Card& c);
+	int decideCard(BasicCard lastCard); // Overloaded
+	bool findCardType(int type, int suit, int& spot);
 	Card playCard(int c);
 };
