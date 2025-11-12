c645e83dca362749181928ba318789a94e124092
ESDunlap <dunlape3@my.erau.edu>
2025-11-11 20:27:30 -0700

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -4,6 +4,7 @@
 #include <string>
 
 using namespace std;
+class Deck;
 
 class Card
 {
@@ -11,7 +12,20 @@ public:
 	virtual int getRank() { return 0; }
 	virtual int getSuit() { return 0; }
 	virtual bool getPlus() { return false; }
-	virtual bool onPlay() { return false; }
+};
+
+class BasicCard : public Card
+{
+private:
+	int rank; //0=0, 1=1...
+	int suit; //red, green, yellow, blue
+	bool plus;
+public:
+	BasicCard(int, string, bool= false);
+	BasicCard(int, int, bool= false);
+	int getRank(){return rank;}
+	int getSuit(){return suit;}
+	bool getPlus(){return plus;}
 };
 
 class WildCard : public Card
@@ -41,23 +55,10 @@ public:
 	void shuffle();
 	void startGame();
 	Card* drawTop();
+	bool onPlay(Card*);
 	~Deck();
 };
 
-class BasicCard : public Card
-{
-private:
-	int rank; //0=0, 1=1...
-	int suit; //red, green, yellow, blue
-	bool plus;
-public:
-	BasicCard(int, string, bool= false);
-	BasicCard(int, int, bool= false);
-	int getRank(){return rank;}
-	int getSuit(){return suit;}
-	bool getPlus(){return plus;}
-	bool onPlay(Deck&);
-};
 
 BasicCard* makeBasicCard(int rank, int suit, bool plus = false);
 
