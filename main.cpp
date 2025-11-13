261455e0401c0da4d3af727b737cc26410c9af35
ESDunlap <dunlape3@my.erau.edu>
2025-11-13 11:59:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,24 +11,72 @@
 #include "Deck.h"
 #include "Player.h"
 
+int getValidInt(string message)
+{
+    int getInt;
+    cout<<message<<endl;
+    cin>>getInt;
+    while(!cin)
+    {
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+        cout<<"Error: Not an Int"<<endl;
+        cin>>getInt;
+    }
+    return getInt;
+}
+
 
 int main()
 {
     srand(time(0));
+    bool someoneWon;
 	Deck newDeck;
-	Player p;
 	newDeck.startGame();
-	p.giveCard(newDeck);
+	/*
+	int numAi= getValidInt("How many AI's will you be fighting? (1-3): ");
+	while (numAi > 3 || numAi < 1)
+	{
+	    cout<<"Error: Outside of Range"<<endl;
+	    numAi= getValidInt("How many AI's will you be fighting? (1-3): ");
+	}
+	*/
+	int numAi=3;
+	int startCards= getValidInt("How many cards will a player start with? (3-10):");
+	while (startCards > 10 || startCards < 3)
+	{
+	    cout<<"Error: Outside of Range"<<endl;
+	    startCards= getValidInt("How many cards will a player start with? (3-10):");
+	}
+	Player allPlayers[numAi+1];
+	for(int i = 0; i<=numAi;i++)
+	{
+	    if (i!=0)
+	        allPlayers[i].setAi();
+	    for(int v = 0; v<startCards; v++)
+	        allPlayers[i].giveCard(newDeck);
+	}
+	int currentPlayer= rand() % (numAi+1);
+	bool reverse= false;
+	bool skip= false;
+	bool plus= false;
 	try
 	{
-	for(int i = 0;i<130;i++)
-	    newDeck.drawTop();
+    	while (!someoneWon)
+    	{
+    	    if(reverse)
+    	        currentPlayer = (currentPlayer+1) % (numAi+1);
+    	    else
+    	        currentPlayer = (currentPlayer-1) % (numAi+1);
+    	    allPlayers[currentPlayer].playTurn(reverse, skip, plus, newDeck);
+    	    if(allPlayers[currentPlayer].getSize() == 0)
+    	        someoneWon=true;
+    	}
 	}
 	catch(long tooSmall)
 	{
 	    cout<<"Playpile and Deck are too small to continue the game"<<endl;
 	}
-	p.playCard(0, newDeck);
 	/*
 	newDeck.fillDeck();
 	newDeck.shuffle();
