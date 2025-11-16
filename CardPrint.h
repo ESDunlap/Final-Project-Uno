c4ccc988fe6ae7026179884abe930226ca11f6c4
Kaeden-D <dalyk3@my.erau.edu>
2025-11-16 15:33:04 -0700

    Fix errors

--- CardPrint.h
+++ CardPrint.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream> //Generic c++
 #include <fstream>
+#include <vector>
 
 #include "Deck.h"
 
@@ -9,3 +10,4 @@ using namespace std;
 string getFileContents(ifstream& File);
 string printAscii(string file);
 ostream& suitColor(ostream& os, int suit);
+string getMultiFileContents(vector<string> files, vector<int> suits);
